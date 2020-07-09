1e390d78e61bdac9a9e5c66cb7071c54394bc98c
apfhd1018 <63091064+apfhd1018@users.noreply.github.com>
2020-07-09 17:32:02 +0900

    Updates

---
+++ precache-manifest.c38aef1ec798ac4c50e7a471eee8bfce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034f437e441c996461f7fa680de6bdcb",
+    "url": "/News/index.html"
+  },
+  {
+    "revision": "45ad910bafc63d9f42c5",
+    "url": "/News/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1aa8dcfe75491993081f",
+    "url": "/News/static/js/2.89bdf43b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/News/static/js/2.89bdf43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ad910bafc63d9f42c5",
+    "url": "/News/static/js/main.4ebf6bba.chunk.js"
+  },
+  {
+    "revision": "fb88da852d9d0d64606e",
+    "url": "/News/static/js/runtime-main.38e99944.js"
+  }
+]);
