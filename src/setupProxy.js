a11483a06c8ae6db94532bdf11f96b472f85dff3
apfhd1018 <63091064+apfhd1018@users.noreply.github.com>
2020-07-09 20:14:15 +0900

    sepupProxy target변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(
-      "/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f",
-      {
-        target: "https://newsapi.org",
-        secure: false,
-        changeOrigin: true,
-      }
-    )
+    createProxyMiddleware("/*", {
+      target: "https://apfhd1018.github.io",
+      changeOrigin: true,
+    })
   );
 };
